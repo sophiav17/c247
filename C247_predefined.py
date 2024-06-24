d2078690fd7073894931b45f9abc88fedfb93563
Sophia <99284300+sophiav17@users.noreply.github.com>
2024-06-24 17:38:14 +0100

    Add files via upload

---
+++ C247_predefined.py
@@ -0,0 +1,13 @@
+
+for nonce in range(5): # set a range for iteration
+	equation = 25-5+nonce
+	if equation == 24:
+		print("verified") #display as "verified" and mention the nonce value at which it got verified. 
+		break
+	else:
+		print("not verified")#display as " not verified"
+
+
+
+
+
